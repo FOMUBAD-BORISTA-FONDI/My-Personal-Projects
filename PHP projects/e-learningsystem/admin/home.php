278517ea259f58803c6599e018c99b125c0c8867
Borista <117987381+FOMUBAD-BORISTA-FONDI@users.noreply.github.com>
2023-08-31 22:18:10 +0100

    E-learning web App with Admin panel

---
+++ PHP projects/e-learningsystem/admin/home.php
@@ -0,0 +1,35 @@
+<style type="text/css">
+ 
+  .secondrow > .row {
+     border: 1px solid #eee;
+    /*padding: 5px 30px;*/
+    text-align: center; 
+    margin: 20px;
+    min-height: 50px;
+     border-radius: 50%;
+  }
+  .imgstretch{ 
+    padding: 10px 30px;
+  }
+  .imgstretch img {
+    width: 100%;
+    height: 100px;
+  }
+
+ 
+</style>
+
+  <div class="btn-controls">
+    <div class="btn-box-row row-fluid">
+        <a href="#" class="btn-box big span4"><i class=" icon-pencil"></i><b>25%</b>
+            <p class="text-muted">
+                Lesson</p>
+        </a><a href="#" class="btn-box big span4"><i class="icon-user"></i><b>35</b>
+            <p class="text-muted">
+                New Users</p>
+        </a><a href="#" class="btn-box big span4"><i class="icon-book"></i><b>1,572</b>
+            <p class="text-muted">
+                Exercises</p>
+        </a>
+    </div> 
+</div>
---
+++ PHP projects/e-learningsystem/home.php
@@ -0,0 +1,97 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>Who we are </h4>
+        <p>
+          CryptoBuja is one of the most well-known and trusted platforms for Crypto
+          Education. The platform's goal is to assist people to make more money, live better
+          and more secure financial lives through crypto currency investment. We have
+          helped a lot of people reach their significant financial objectives more efficiently
+          and promptly with our innovative crypto investment strategies. Our staff is made
+          up of highly skilled, knowledgeable, and experienced individuals who are always
+          able to show clients the proper path based on their requirements and
+          preferences.</p>
+  </div>
+<div  class="col-lg-6">
+  <h4>Our Vision</h4> 
+  <p>Our vision is to empower our customers to succeed in every aspect of their crypto
+investment lives, be it entry or expert levels. Our community driven culture and
+ongoing commitment to superior service, innovative connectivity and intuitive
+technology will expand our relationships for generations to come. Our goal is to
+dominate the internet crypto currency space with top notch services.</p>
+  <h4>Our Mission</h4>
+  <p> CryptoBuja aims at creating innovative crypto services and solutions that satisfy
+the demands of both entry and expert level crypto investors worldwide with the
+help of our experts and advanced technologies. We will continue to become a
+platform where crypto investors will get everything they want like the best crypto
+education, reliability, transparency, best affiliate program and useful strategies to
+make a success in the crypto verse.</p>
+  
+<h4>Core Values</h4> 
+  <ol>
+    <li>Integrity
+      <ul>Everyone who represents our organization is committed to acting in a way that is
+consistent with the company's values.</ul>
+    </li>
+    <li>Honesty
+      <ul>Not only the optimal course of action. We are honest and trustworthy in our
+actions, winning the respect of our coworkers, clients, and the general public.</ul>
+    </li>
+    <li>Accountability
+      <ul>We are the best when it comes to establishing trust both internally and publicly
+taking ownership of actions (and inactions).</ul>
+    </li>
+    <li>Customer commitment
+      <ul>Being sincere with our words and the connections we build is the first step in
+providing excellent customer service.</ul>
+    </li>
+    <li>Inclusion and Diversity
+      <ul>As an Organization, we are expanding by integrating a variety of backgrounds and
+life experiences into a setting where everyone has an equal chance.</ul>
+    </li>
+    <li>Learning 
+      <ul>No one knows every solution. A fundamental tenet of successful businesses is a
+culture of modesty with ongoing learning which we highly practice.</ul>
+    </li>
+    <li>Team Work 
+      <ul>People are capable of building something bigger than themselves when they
+collaborate. Our team of qualified professionals is excellent with collaboration and
+providing the best results.</ul>
+    </li>
+     
+  </ol>
+</div>
+<!-- <div  class="col-lg-6">
+  <h4>Guiding Principles</h4> 
+    <ul>
+      <li>Academic Freedom </li>
+      <li>Responsibility</li>
+      <li>Academic Standards</li>  
+    </ul>
+</div> -->
+
+<div  class="col-lg-6">
+  <h4>Road Map</h4> 
+    <p>In order to provide consumers with a variety of crypto investment strategies
+throughout the world, CryptoBuja was founded in 2022 with the goal of
+establishing itself as the top Crypto Education and investment platform. Being
+superior entails offering excellent client service and the finest prices too.</p>
+</div>
+
+<div  class="col-lg-6">
+  <h4>Why Us?</h4> 
+    <p>CryptoBuja uses the most advanced technology and provides the best crypto
+Investment strategies and solutions through a rigorous planning procedure. Added
+to our crypto education, we also provide a flexible affiliate marketing plan for our
+subscribers.</p>
+<h4>Expectations</h4> 
+    <ul>
+      <li>Financial Stability</li>
+      <li>All Crypto related info</li>
+      <li>Crypto Exchange</li>
+      <li>Crypto Trading Overview</li> 
+    </ul>
+</div>
+
+ 
+
